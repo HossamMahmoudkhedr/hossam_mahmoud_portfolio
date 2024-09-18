43f96112d11ce4a6cd09d55f6397e07b7ca94247
HossamMahmoudkhedr <nhem318@gmail.com>
2024-09-18 17:27:26 +0300

    modify footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -75,7 +75,7 @@ const Footer = () => {
 						/>
 					</a>
 					<a
-						href="https://twitter.com/HossamKhedr01"
+						href="https://x.com/hossam_mahmoudd"
 						target="_blank"
 						rel="noreferrer">
 						<img
