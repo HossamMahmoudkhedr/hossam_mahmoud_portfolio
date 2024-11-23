a3736596e6c9e05afb3d4018a403c01d739b9ba2
Hossam Mahmoud <76559078+HossamMahmoudkhedr@users.noreply.github.com>
2024-11-23 23:23:10 +0200

    Update educationData.js

--- src/data/educationData.js
+++ src/data/educationData.js
@@ -4,7 +4,7 @@ export const edData = [
 		name: 'Bachelor in Computer Science',
 		type: 'Damanhour University',
 		about: {
-			date: 'September 2020 - Present (Excepected graduation: July 2024)',
+			date: 'September 2020 - June 2024',
 			info: 'During my time at the faculty of computer science, I gained a solid foundation in computer science and acquired essential knowledge and skills in various programming languages, algorithms, data structures, and software development methodologies.<br><br> The rigorous coursework and hands-on projects equipped me with the ability to tackle complex problems and develop innovative solutions.<br><br>I actively participated in team projects, collaborating with fellow students to design and implement software applications. These experiences fostered my teamwork and communication skills while honing my ability to work effectively in a collaborative environment.',
 			subTitle: '',
 			list: [],
